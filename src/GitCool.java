82fa7e42e7fefbfd8416197c7a1788e1c83ce8a2
walid01994 <abidwalid1994@gmail.com>
2021-07-19 16:48:45 -0400

    more line on the class

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,9 @@ public class GitCool {
     // B23 is learning Git
     // B23 is nice
     // B23 is special
+
+
+    // we have succefully been able to add our Github account
+    // share project on Github
+    // Create remote repository under my account
 }
